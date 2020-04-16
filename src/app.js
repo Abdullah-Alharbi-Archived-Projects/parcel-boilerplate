9acee71db6e9b5f888bb2ce960d02b74dafd8f31
W2AlharbiMe <w2.alharbi.me@gmail.com>
2020-04-16 08:36:39 +0300

    updated the entry point

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+// jsx custom compiler
+// note: babel will call this function
+import compile from "./compiler";
+window.compile = compile; // add the compile function to the global object browser
+
+// app component
+import App from "./components/App";
+
 // Bootstrap
 import "bootstrap";
-import "./scss/app.scss"; // scss entry point
+
+// scss entry point
+import "./scss/app.scss";
+
+(async () => {
+  // inject app component in #app you can find it at public/index.html
+  document.getElementById("app").appendChild(App());
+})();
