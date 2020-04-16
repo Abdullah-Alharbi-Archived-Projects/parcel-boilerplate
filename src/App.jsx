24cdc33e5fc2a932da4e64c42ef7949fb98eb26c
W2AlharbiMe <w2.alharbi.me@gmail.com>
2020-04-16 09:35:34 +0300

    initial page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
 const App = () => (
-  <div className="app">
-    <h1>Hello, World</h1>
+  <div className="d-flex h-100 flex-column justify-content-center align-items-center">
+    <h1>Parcel Boilerplate</h1>
+
+    <div>
+      <h3>Features:</h3>
+      <ol>
+        <li>Uses Bootstrap 4</li>
+        <li>Configured with Scss</li>
+        <li>Similar to react</li>
+        <li>Uses Modren Javascript development</li>
+        <li>Uses DOM Directly</li>
+        <li>Super fast</li>
+        <li>Uses Jsx</li>
+        <li>You can customize bootstrap 4 through scss</li>
+      </ol>
+    </div>
+
+    <div>
+      <a
+        href="https://github.com/W2AlharbiMe/parcel-boilerplate"
+        target="_blank"
+      >
+        Github Repository
+      </a>
+    </div>
   </div>
 );
 
